b79eb6e64eb396befc7cf6d7a279b36a7c893117
Sergeev Sergey <Sergeev_sergey99@mail.ru>
2026-04-06 06:23:07 +0300

    дофикс пятого примера

--- Examples/Demo5 Containers/Scripts/Bindings/ContainerInventoryDataBinding.cs
+++ Examples/Demo5 Containers/Scripts/Bindings/ContainerInventoryDataBinding.cs
@@ -5,6 +5,7 @@ using DragAndDropSystem.Inspector;
 using DragAndDropSystem.Inventories;
 using DragAndDropSystem.Examples.Containers.UI;
 using DragAndDropSystem.Rules;
+using DragAndDropSystem.Slots;
 using UnityEngine;
 using UnityEngine.Serialization;
 
@@ -14,11 +15,45 @@ namespace DragAndDropSystem.Examples.Containers
     {
         public ContainerItemInstance currentContainer { get; private set; }
 
-        #region Overrides
         protected override IReadOnlyList<IContainerizeItemInstance> GetItems() => currentContainer.Items;
         protected override ContainerItemAdapterAdapter CreateAdapter(IContainerizeItemInstance item) => new(item);
         protected override void AddToData(ContainerItemAdapterAdapter adapterAdapter) => currentContainer.AddItem(adapterAdapter.Instance);
         protected override void RemoveFromData(ContainerItemAdapterAdapter adapterAdapter) => currentContainer.RemoveItem(adapterAdapter.Instance);
+        
+        protected override bool CanHandleOccupiedSlotDrop(DragEntry entry, ISlot occupiedSlot)
+        {
+            if (occupiedSlot?.Stack?.PrimaryAdapter is not ContainerItemAdapterAdapter { Instance: ContainerItemInstance container })
+                return false;
+
+            if (entry.Stack?.PrimaryAdapter is not ContainerItemAdapterAdapter sourceAdapter)
+                return false;
+
+            if (container.Items.Count >= container.Item.Capacity)
+                return false;
+
+            if (sourceAdapter.Instance is ContainerItemInstance dragged && WouldCreateCycle(dragged, container))
+                return false;
+
+            return true;
+        }
+
+        protected override bool ExecuteOccupiedSlotDrop(DragEntry entry, ISlot occupiedSlot)
+        {
+            if (occupiedSlot?.Stack?.PrimaryAdapter is not ContainerItemAdapterAdapter { Instance: ContainerItemInstance container })
+                return false;
+
+            if (entry.Stack?.PrimaryAdapter is not ContainerItemAdapterAdapter sourceAdapter)
+                return false;
+
+            container.AddItem(sourceAdapter.Instance);
+            ContainerDemoManager.AutoCreateInstance.RemovePlayerItem(sourceAdapter.Instance);
+
+            entry.SourceSlot.Clear();
+            entry.SourceSlot.UpdateVisuals();
+
+            Events.InvokeContainerContentChanged(container);
+            return true;
+        }
         
         protected override RuleResult CanDrop(DragContext context, DragEntry entry)
         {
@@ -27,7 +62,6 @@ namespace DragAndDropSystem.Examples.Containers
 
             return base.CanDrop(context, entry);
         }
-        #endregion
         
         public void SetContainer(ContainerItemInstance container)
         {
