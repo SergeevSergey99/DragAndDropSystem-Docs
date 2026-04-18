28b543d1f790640040e4116c40a8713905416e8e
Sergeev Sergey <Sergeev_sergey99@mail.ru>
2026-04-18 03:13:54 +0300

    промежуточный класс FadeTooltipView

--- Scripts/UI/Tooltip/DefaultTooltipView.cs
+++ Scripts/UI/Tooltip/DefaultTooltipView.cs
@@ -13,7 +13,7 @@ namespace UniversalDragAndDrop.UI
     /// Custom visualizations can be created by implementing ITooltipView.
     /// </summary>
     [RequireComponent(typeof(RectTransform))]
-    public class DefaultTooltipView : BaseTooltipView
+    public class DefaultTooltipView : FadeTooltipView
     {
         [Header("UI Elements")]
         [SerializeField, Tooltip("Item name text")] 
@@ -28,61 +28,6 @@ namespace UniversalDragAndDrop.UI
 
         [SerializeField, Tooltip("Item icon")]
         private Image _itemIcon;
-
-        [SerializeField, Tooltip("CanvasGroup for animation")]
-        private CanvasGroup _canvasGroup;
-
-        [SerializeField, Tooltip("Fade-in speed")]
-        private float _fadeInTime = 1f;
-
-        [SerializeField, Tooltip("Fade-out speed")]
-        private float _fadeOutTime = 1f;
-
-        protected void Awake()
-        {
-            _canvasGroup = GetComponent<CanvasGroup>();
-            if (_canvasGroup == null)
-            {
-                _canvasGroup = gameObject.AddComponent<CanvasGroup>();
-            }
-            _canvasGroup.alpha = 0f;
-        }
-
-        protected override void ShowView(Action OnCompleted)
-        {
-            gameObject.SetActive(true);
-
-            // Fade-in animation
-            if (_canvasGroup != null)
-            {
-                _canvasGroup.FadeTo(1f, _fadeInTime, OnCompleted);
-            }
-            else
-            {
-                if (_canvasGroup != null)
-                    _canvasGroup.alpha = 1f;
-
-                OnCompleted?.Invoke();
-            }
-        }
-
-        protected override void HideView(Action OnCompleted)
-        {
-            // Fade-out animation
-            if (_canvasGroup != null && gameObject.activeSelf)
-            {
-                _canvasGroup.FadeTo(0f, _fadeOutTime, () => {
-                        gameObject.SetActive(false);
-                        OnCompleted?.Invoke();
-                    });
-            }
-            else
-            {
-                // Hide immediately
-                gameObject.SetActive(false);
-                OnCompleted?.Invoke();
-            }
-        }
 
         /// <summary>
         /// Fill tooltip content
