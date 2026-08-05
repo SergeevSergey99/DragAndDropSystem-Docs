de73c57cba1d71f457e40df22177fe46daeff9c4
Sergeev Sergey <Sergeev_sergey99@mail.ru>
2026-08-06 00:46:29 +0300

    Crossed slot

---
+++ Scripts/Slots/CrossFeedbackSlot.cs
@@ -0,0 +1,27 @@
+﻿using UDND.Inventories;
+using UnityEngine;
+
+namespace UDND.Slots
+{
+    public class CrossFeedbackSlot : UniversalSlot
+    {
+        [SerializeField] private GameObject _redCross;
+
+        public override void Highlight(bool highlight)
+        {
+            base.Highlight(highlight);
+            _redCross.SetActive(highlight && !AcceptsCurrentDrag());
+        }
+
+        private bool AcceptsCurrentDrag()
+        {
+            var manager = DragAndDropManager.AutoCreateInstance;
+            if (!manager.IsDragging || Inventory is not IInventoryInteraction interaction)
+                return true;
+
+            return interaction.TryGetDropPreviewSlots(
+                       this, manager.CurrentContext, out _, out bool canPlace)
+                   && canPlace;
+        }
+    }
+}
